53632fb91d384e101f73294894293b66274fa381
Driann's <114148680+Driannm@users.noreply.github.com>
2026-06-15 19:57:49 +0700

    feat(database): add SQLite database client factory

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,12 +1,10 @@
 import { drizzle, BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
-import * as schema from './schema.js';
-
-export * from './schema.js';
+import * as sqliteSchema  from './schema.sqlite.js';
 
 // Definisikan interface return type secara eksplisit untuk mencegah TS4058
 export interface DatabaseClient {
-  db: BetterSQLite3Database<typeof schema>;
+  db: BetterSQLite3Database<typeof sqliteSchema >;
   sqlite: Database.Database;
 }
 
@@ -16,7 +14,11 @@ export function createDatabaseClient(databasePath: string): DatabaseClient {
   sqlite.pragma('synchronous = NORMAL');
   
   return {
-    db: drizzle(sqlite, { schema }),
+    db: drizzle(sqlite, { schema: sqliteSchema }),
     sqlite,
   };
 }
+
+export * from './schema.sqlite.js';
+export * from './schema.pg.js';
+export * from './queries.js';
